131002777460a7d53dc892cff866abe20665114e
Taylor Stackpole <tay.stack@gmail.com>
2019-01-27 17:17:11 -0800

    Removing coveralls

--- scripts/publish_coverage.sh
+++ scripts/publish_coverage.sh
@@ -5,7 +5,5 @@ echo """
   Publishing Coverage
 
 """
-./node_modules/.bin/jest --coverage && \
-cat ./coverage/lcov.info | ./node_modules/.bin/coveralls
 
 ./node_modules/.bin/codecov --token="$CODECOV_TOKEN_JS_COUNTER"
